fa171b7faa333a902beda3922e29aadc235a2e03
Pedro Albino <henriquealbino1@gmail.com>
2026-09-11 13:00:01 -0300

    propriedades calculadas

--- src/main/java/com/albinos/ordering/domain/entity/OrderItem.java
+++ src/main/java/com/albinos/ordering/domain/entity/OrderItem.java
@@ -38,7 +38,7 @@ public class OrderItem {
     @Builder(builderClassName = "BrandNewOrderItemBuilder", builderMethodName = "brandNew")
     private static OrderItem createBrandNew(OrderId orderId, ProductId productId,
                      ProductName productName, Money price, Quantity quantity) {
-        return new OrderItem(
+        OrderItem orderItem = new OrderItem(
                 new OrderItemId(),
                 orderId,
                 productId,
@@ -46,8 +46,9 @@ public class OrderItem {
                 price,
                 quantity,
                 Money.ZERO
-
         );
+        orderItem.recalculateTotals();
+        return orderItem;
     }
 
 
@@ -78,6 +79,10 @@ public class OrderItem {
 
     public Money totalAmount() {
         return totalAmount;
+    }
+
+    private void recalculateTotals(){
+        this.setTotalAmount(this.price().multiply(this.quantity));
     }
 
     private void setId(OrderItemId id) {
--- src/test/java/com/albinos/ordering/domain/entity/OrderTest.java
+++ src/test/java/com/albinos/ordering/domain/entity/OrderTest.java
@@ -1,14 +1,89 @@
 package com.albinos.ordering.domain.entity;
 
 
+import com.albinos.ordering.domain.valueobject.Money;
+import com.albinos.ordering.domain.valueobject.ProductName;
+import com.albinos.ordering.domain.valueobject.Quantity;
 import com.albinos.ordering.domain.valueobject.id.CustomerId;
+import com.albinos.ordering.domain.valueobject.id.ProductId;
+
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.mockito.internal.matchers.Or;
+
+import java.util.Set;
 
 class OrderTest {
 
     @Test
     public void shouldGenerate(){
         Order order = Order.draft(new CustomerId());
+    }
+
+    @Test
+    public void shouldAddItem() {
+        Order order = Order.draft(new CustomerId());
+        ProductId productId = new ProductId();
+
+        order.addItem(
+                productId,
+                new ProductName("Mouse pad"),
+                new Money("100"),
+                new Quantity(1)
+        );
+
+        Assertions.assertThat(order.items().size()).isEqualTo(1);
+
+        OrderItem orderItem = order.items().iterator().next();
+
+        Assertions.assertWith(orderItem,
+                (i) -> Assertions.assertThat(i.id()).isNotNull(),
+                (i) -> Assertions.assertThat(i.productName()).isEqualTo(new ProductName("Mouse pad")),
+                (i) -> Assertions.assertThat(i.productId()).isEqualTo(productId),
+                (i) -> Assertions.assertThat(i.price()).isEqualTo(new Money("100")),
+                (i) -> Assertions.assertThat(i.quantity()).isEqualTo(new Quantity(1))
+        );
+    }
+
+    @Test
+    public void shouldGenerateExceptionWhenTryToChangeItemSet() {
+        Order order = Order.draft(new CustomerId());
+        ProductId productId = new ProductId();
+
+        order.addItem(
+                productId,
+                new ProductName("Mouse pad"),
+                new Money("100"),
+                new Quantity(1)
+        );
+
+        Set<OrderItem> items = order.items();
+
+        Assertions.assertThatExceptionOfType(UnsupportedOperationException.class)
+                .isThrownBy(items::clear);
+    }
+
+    @Test
+    public void shouldCalculateTotals() {
+        Order order = Order.draft(new CustomerId());
+        ProductId productId = new ProductId();
+
+        order.addItem(
+                productId,
+                new ProductName("Mouse pad"),
+                new Money("100"),
+                new Quantity(2)
+        );
+
+        order.addItem(
+                productId,
+                new ProductName("RAM Memory"),
+                new Money("50"),
+                new Quantity(1)
+        );
+
+        Assertions.assertThat(order.totalAmount()).isEqualTo(new Money("250"));
+        Assertions.assertThat(order.totalItems()).isEqualTo(new Quantity(3));
     }
 
 }
